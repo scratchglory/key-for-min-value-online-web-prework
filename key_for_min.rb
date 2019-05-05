cabbb4fc4b2e3960fb0c2496b6558755a3a1c5b5
Adella <adellakenn@gmail.com>
2019-05-05 16:50:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,20 +3,6 @@
 # {:blake=>500, :ashley=>2, :adam=>1}
 require 'pry'
 
-# def key_for_min_value(name_hash)
-#   min_value = nil
-#   min_key = nil
-#   no_value = nil
-#   name_hash.each do |key, value|
-#     if min_value == nil || value < min_value
-#       key = value
-#     end
-#   end
-#   # min_value
-# # binding.pry
-# end
-
-
 
 
 def key_for_min_value(name_hash)
@@ -24,9 +10,9 @@ def key_for_min_value(name_hash)
   smallest = 1000000
 
   name_hash.each do |key, value|
-    if value <= smallest    # 500 <= 10000   # 2<= 10000  # 1 <= 10000
-      smallest = value			#  					 #
-      smallest_name = key   #  :blake
+    if value <= smallest
+      smallest = value	
+      smallest_name = key
     end
   end
   smallest_name
