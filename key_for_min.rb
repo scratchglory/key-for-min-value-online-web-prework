8ceff7ed47a9754e8a1309481051da1009c714c0
Adella <adellakenn@gmail.com>
2019-05-05 16:48:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -17,8 +17,8 @@ require 'pry'
 # end
 
 
-empty = {}
-name_hash = {:blake => 500, :ashley => 2, :adam => 1}
+# empty = {}
+# name_hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(name_hash)
   smallest_name = nil
