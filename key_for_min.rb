54b291396a09302e7eca2f5153ed17c9d7acfeef
Adella <adellakenn@gmail.com>
2019-05-05 16:16:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,9 @@ require 'pry'
 
 def key_for_min_value(name_hash)
   min_value = []
-  no_value = nil
+ 
   name_hash.each do |key, value|
-    if value == no_value
+    if value == value.nil
       min_value = key
     end
   end
